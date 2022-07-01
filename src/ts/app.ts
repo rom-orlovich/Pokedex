10bef2269bb92e9e7d48e6553d18aa54647a264b
rom-orlovich <rom-orlovich@cyber4s.dev>
2022-07-02 00:21:25 +0300

    create load data after ui branch

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,11 +1,9 @@
 import { PokemonsData } from "./PokemonsData";
 import { HomePage } from "./pages/HomePage";
-// import { DataStorage } from "./DataStorage";
 
-export async function initApp() {
+export function initApp() {
   const dataPokemons = new PokemonsData();
   const homePage = new HomePage(dataPokemons);
   document.body.appendChild(homePage.render());
-
   homePage.initEvents();
 }
