19ac8fc316cd08aacf794b3451fa69d54594e49e
rom-orlovich <rom-orlovich@cyber4s.dev>
2022-07-02 22:40:04 +0300

    add alert message to click on the logo to active the music

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,7 +1,9 @@
 import { PokemonsData } from "./PokemonsData";
 import { HomePage } from "./pages/HomePage";
 
+alert("Click on the logo!");
 // Init the app
+
 export function initApp() {
   const dataPokemons = new PokemonsData();
   const homePage = new HomePage(dataPokemons);
