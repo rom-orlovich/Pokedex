426d9f790ef47d1b889597c7160607267e1eb800
rom-orlovich <rom-orlovich@cyber4s.dev>
2022-07-03 00:54:09 +0300

    add comment play the music

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,8 +1,7 @@
 import { PokemonsData } from "./PokemonsData";
 import { HomePage } from "./pages/HomePage";
 
-// Init the app
-
+// Init the app - click the logo to play the music!
 export function initApp() {
   const dataPokemons = new PokemonsData();
   const homePage = new HomePage(dataPokemons);
