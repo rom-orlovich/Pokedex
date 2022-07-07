a1702d94d5098f187896fb19397c4f559458f4e5
rom-orlovich <rom-orlovich@cyber4s.dev>
2022-07-07 14:08:43 +0300

    check the server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import crypto from "crypto";
+// import crypto from "crypto";
 import cors from "cors";
 
 const app = express();
@@ -7,7 +7,11 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.get("/check", (req: Request, res: Response) => {
+  console.log("check");
 
+  res.send("ok");
+});
 app.listen(5000, () => {
   console.log("listen port 5000");
 });
