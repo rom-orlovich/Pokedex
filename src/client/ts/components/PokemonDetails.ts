3328e9d1594d5337142c519d786c40bc4ebe0407
rom-orlovich <rom-orlovich@cyber4s.dev>
2022-07-09 15:00:44 +0300

    delete unnecssery files

--- src/client/ts/components/PokemonDetails.ts
+++ src/client/ts/components/PokemonDetails.ts
@@ -39,16 +39,3 @@ export class PokemonsDetails {
     return li;
   }
 }
-
-// $(document).ready(function () {
-// $("#heart").click(function () {
-// if ($("#heart").hasClass("liked")) {
-// $("#heart").html('<i class="fa fa-heart-o" aria-hidden="true"></i>');
-// $("#heart").removeClass("liked");
-// } else {
-// $("#heart").html('<i class="fa fa-heart" aria-hidden="true"></i>');
-// $("#heart").addClass("liked");
-// }
-// });
-// });
-//
