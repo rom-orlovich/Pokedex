9a54d6fbb3a1f7b0d4163d65fd077d5f1c1e6280
rom-orlovich <rom-orlovich@cyber4s.dev>
2022-07-10 21:23:36 +0300

    success implement webpack build

--- src/server/utlites/constansVariables.ts
+++ src/server/utlites/constansVariables.ts
@@ -1,17 +1,16 @@
 import { join } from "./fsHelpers";
 
 export const FAVORITE_POKEMONS_FILENAME = "favoritePokemons.json";
+
+const dbFolder =
+  process.env.NODE_ENV === "production"
+    ? "./public/server/db"
+    : join(__dirname, "../", "db");
+
 export const FAVORITE_POKEMONS_PATH = join(
-  __dirname,
-  "../",
-  "db",
+  dbFolder,
   FAVORITE_POKEMONS_FILENAME
 );
 
 export const POKEMONSDB_FILENAME = "pokemonsDB.json";
-export const POKEMONS_DB_PATH = join(
-  __dirname,
-  "../",
-  "db",
-  POKEMONSDB_FILENAME
-);
+export const POKEMONS_DB_PATH = join(dbFolder, POKEMONSDB_FILENAME);
