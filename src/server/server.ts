ccd53faa800fc7318b831ea89e1535d0b8491a9a
rom-orlovich <rom-orlovich@cyber4s.dev>
2022-07-10 09:35:34 +0300

    some change to backend

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use("/", pokemonsDataRoutes);
 
