41b1ccac4710266f32eb276b756283e20f3f34e0
rom-orlovich <rom-orlovich@cyber4s.dev>
2022-07-10 17:27:05 +0300

    add somechanges

--- src/server/utlites/constansVariables.ts
+++ src/server/utlites/constansVariables.ts
@@ -1,17 +1,9 @@
 import { join } from "./fsHelpers";
 
 export const FAVORITE_POKEMONS_FILENAME = "favoritePokemons.json";
-export const FAVORITE_POKEMONS_PATH = join(
-  __dirname,
-  "../",
-  "db",
-  FAVORITE_POKEMONS_FILENAME
-);
+const dbPath = join(__dirname, "../", "db");
+export const FAVORITE_POKEMONS_PATH = join(dbPath, FAVORITE_POKEMONS_FILENAME);
 
 export const POKEMONSDB_FILENAME = "pokemonsDB.json";
-export const POKEMONS_DB_PATH = join(
-  __dirname,
-  "../",
-  "db",
-  POKEMONSDB_FILENAME
-);
+
+export const POKEMONS_DB_PATH = join(dbPath, POKEMONSDB_FILENAME);
