739f3a39b4140a6cae5ef05967ce922e9c6c18c4
rom-orlovich <rom-orlovich@cyber4s.dev>
2022-07-10 22:22:58 +0300

    change config url

--- src/client/ts/utlites/constantVariables.ts
+++ src/client/ts/utlites/constantVariables.ts
@@ -6,10 +6,17 @@ export enum EndPointsAPI {
   getFavoritePokemons = "getFavoritePokemons",
   saveFavoritePokemons = "saveFavoritePokemons",
 }
-const port = process.env.PORT || 5000;
+const CONFIG_URL = {
+  production: "https://agile-scrubland-36038.herokuapp.com",
+  development: "http://localhost:5000",
+};
+
 export const POKEMONS_LIST_KEY = "pokemons_list";
 export const POKEMONS_FAVORITE_LIST_KEY = "pokemons_favorite_list";
-export const EXPRESS_SERVER_URL = `http://localhost:${port}`;
+export const EXPRESS_SERVER_URL =
+  process.env.NODE_ENV === "production"
+    ? CONFIG_URL.production
+    : CONFIG_URL.development;
 
 export const GET_ALL_POKEMONS_URL = createApiEndPoint(
   EXPRESS_SERVER_URL,
