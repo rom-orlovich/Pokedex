28f357fac88d22aa1cd47124397c21a63f0edf0e
rom-orlovich <rom-orlovich@cyber4s.dev>
2022-07-15 11:00:53 +0300

    before change routes of endPointapi

--- src/client/ts/components/Heading.ts
+++ src/client/ts/components/Heading.ts
@@ -11,7 +11,7 @@ export class Header {
     
     <div class="heading_pokedex">${createImg(
       "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png",
-      //  [],
+      [],
       "pokemon_heading"
     )}
     <audio src="${audio}"></audio>
